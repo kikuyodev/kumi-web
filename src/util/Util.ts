c8b20c5f5ecdf3f3a5777b648d559d19f17077b4
ryoshu <kyoko.laica@gmail.com>
2023-11-12 16:16:57 -0500

    Allow for search queries in CDN This is primarily used for optimization with the web browser.

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,5 +1,13 @@
 export class Util {
-    public static getCdnFor(location: string, id?: string | number) {
-        return new URL(`/cdn/${location}${id ? `/${id}` : ""}`, import.meta.env.KUMI_API_URL).toString();
+    public static getCdnFor(location: string, id?: string | number, settings?: Record<string, string | number | boolean>) {
+        const url = new URL(`/cdn/${location}${id ? `/${id}` : ""}`, import.meta.env.KUMI_API_URL);
+
+        if (settings) {
+            for (const [key, value] of Object.entries(settings)) {
+                url.searchParams.set(key, value.toString());
+            }
+        }
+
+        return url.toString();
     }
 }

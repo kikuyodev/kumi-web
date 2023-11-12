1d1588241710d3d4e79f68ef5d1f3aff1ecfc93e
ryoshu <kyoko.laica@gmail.com>
2023-11-12 16:19:33 -0500

    Adjust chart background and split its CSS file This also adjusts the height of the background (which the scrollbar will have to be fixed alongside narrow browsers, or mobile phones)

--- src/components/charts/ChartBackground.tsx
+++ src/components/charts/ChartBackground.tsx
@@ -1,6 +1,8 @@
 import { Accessor, For } from "solid-js";
 import { ApiChart, ApiChartSet } from "../../structures/api/ApiChartSet";
+import { Colors } from "../../util/Colors";
 import { Util } from "../../util/Util";
+import "../../styles/components/charts/chartBackground.scss";
 
 export interface ChartBackgroundProps {
     set?: ApiChartSet;
@@ -9,37 +11,36 @@ export interface ChartBackgroundProps {
 
 export function ChartBackground(props: ChartBackgroundProps) {
     return (
-        <div class="chart--content-details--background" style={{
-            "background-image": `url(${Util.getCdnFor("backgrounds", props.set?.id)}`
-        }}>
-            <div class="chart--content-details--background-info">
-                <div class="chart--content-details--title">{props.set?.title}</div>
-                <div class="chart--content-details--artist">{props.set?.artist}</div> {/* TODO: have settings to prefer original metadata*/}
-                <div class="chart--content-details--creator">
-                    Created by <a href="#">{props.set?.creator.username}</a>
+        <div class="chart_background">
+            <div class="chart_background--background">
+                <img src={Util.getCdnFor("backgrounds", props.set?.id, { format: "chartinfo" })} alt="chartinfo background" loading="lazy" />
+                <div class="chart_background--background-overlay" />
+            </div>
+            <div class="chart_background--info">
+                <div class="chart_background--info-title">{props.set?.title}</div>
+                <div class="chart_background--info-artist">{props.set?.artist}</div> {/* TODO: have settings to prefer original metadata*/}
+                <div class="chart_background--info-creator">
+                    Created by <a href={`/users/${props.set?.creator.id}`}>{props.set?.creator.username}</a>
                 </div>
             </div>
-    
-            <div class="chart--content-details--background-difficulties">
+            <div class="chart_background--difficulties">
                 <For each={props.set?.charts}>
                     {chart => <button
-                        class={`chart--content-details--background-difficulty${chart.id === props.currentChart()?.id
-                            ? " chart--content-details--background-difficulty-selected"
+                        class={`chart_background--difficulties-difficulty ${chart.id === props.currentChart()?.id
+                            ? "chart_background--difficulties-difficulty-selected"
                             : ""}`}
-    
                         style={{
-                            background: "white"
+                            background: Colors.difficultyColorFor(props.currentChart()?.difficulty.difficulty ?? 0),
+                            color: Colors.difficultyTextColorFor(props.currentChart()?.difficulty.difficulty ?? 0)
                         }}
                     >
-                        {chart.difficulty.difficulty}
+                        {(props.currentChart()?.difficulty.difficulty ?? 0).toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                     </button>}
                 </For>
-    
             </div>
-            <div class="chart--content-details--background-bar" style={{
-                /* TODO: difficulty spectrum color */
-                background: "white"
+            <div class="chart_background--bar" style={{
+                background: Colors.difficultyColorFor(props.currentChart()?.difficulty.difficulty ?? 0)
             }} />
-        </div>
+        </div >
     );
 }

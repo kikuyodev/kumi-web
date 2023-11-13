282b7d36ef9aac084093a78470bd8d9d7d3fcf42
ryoshu <kyoko.laica@gmail.com>
2023-11-12 20:52:29 -0500

    Move ApiAccess out of api/

---
+++ src/structures/ApiAccess.ts
@@ -0,0 +1,50 @@
+import { ApiAccount } from "./api/ApiAccount";
+import { ApiChartHistory } from "./api/ApiChartHistory";
+import { ApiChartSet } from "./api/ApiChartSet";
+import { ApiModdingPost } from "./api/ApiModdingPost";
+import { RestClient } from "../util/RestClient";
+
+export class ApiAccess {
+    private _restClient: RestClient;
+
+    constructor() {
+        this._restClient = new RestClient();
+    }
+
+    get rest() {
+        return this._restClient;
+    }
+
+    public async getAccount(id: string | number) {
+        const response = await this.rest.send<["account"], [ApiAccount]>(`/api/v1/accounts/${id}`, {
+            method: "GET",
+            credentials: "include"
+        });
+
+        if (response.code === 200) {
+            return response.data!.account;
+        }
+    }
+
+    public async getChartSet(id: string | number) {
+        const response = await this.rest.send<["set"], [ApiChartSet]>(`/api/v1/chartsets/${id}`, {
+            method: "GET",
+            credentials: "include"
+        });
+
+        if (response.code === 200) {
+            return response.data!.set;
+        }
+    }
+
+    public async getModdingData(id: string | number) {
+        const response = await this.rest.send<["posts", "events"], [ApiModdingPost[], ApiChartHistory[]]>(`/api/v1/chartsets/${id}/modding`, {
+            method: "GET",
+            credentials: "include"
+        });
+
+        if (response.code === 200) {
+            return response.data!;
+        }
+    }
+}

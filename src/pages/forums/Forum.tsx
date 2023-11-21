90aa92227c685df59b8d2faa1fcedc695e3ff929
ryoshu <kyoko.laica@gmail.com>
2023-11-21 14:52:38 -0500

    Show indicator only on unread threads

--- src/pages/forums/Forum.tsx
+++ src/pages/forums/Forum.tsx
@@ -3,18 +3,34 @@ import { ForumsBreadcrumbs } from "../../components/forums/ForumBreadcrumbs";
 import { ForumHeader } from "../../components/forums/ForumHeader";
 import { useApi } from "../../contexts/ApiAccessContext";
 import "../../styles/pages/forums/forum.scss";
-import Fa from "solid-fa";
-import { faCommentAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Fa } from "solid-fa";
+import { faCommentAlt as faCommentAltSolid, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCommentAlt as faCommentAltRegular } from "@fortawesome/free-regular-svg-icons";
 import { SegmentedControl } from "../../components/controls/SegmentedControl";
 import { ApiThread } from "../../structures/api/ApiForum";
-import { For, Show } from "solid-js";
+import { Accessor, For, Show, createMemo } from "solid-js";
 import { AccountFlyout } from "../../components/flyouts/AccountFlyout";
 import { Util } from "../../util/Util";
+import { PaginationMeta } from "../../util/api/ApiResponse";
+
+interface ForumMeta extends PaginationMeta {
+    threads: {
+        is_read: boolean;
+    }[]
+}
 
 export function Forum() {
     const params = useParams();
     const forum = useApi(async api => await api.forum.getForum(params.id));
     const threads = useApi(async api => await api.forum.getForumThreads(params.id));
+
+    const meta = createMemo(() => {
+        if (!threads()?.meta) {
+            return;
+        }
+
+        return threads()?.parseMeta<ForumMeta>();
+    });
 
     return <div class="forum">
         <div class="forum--background">
@@ -56,7 +72,7 @@ export function Forum() {
                             </div>
                         </div>
                         <div class="forum--content-body-content-section-list">
-                            <For each={threads()?.data?.threads}>{thread => <Thread {...thread} />}</For>
+                            <For each={threads()?.data?.threads}>{thread => <Thread thread={thread} meta={meta} />}</For>
                         </div>
                     </div>
                 </div>
@@ -65,22 +81,40 @@ export function Forum() {
     </div>;
 }
 
-function Thread(props: ApiThread) {
-    return <a href={`/forums/threads/${props.id}`} class="forum--content-body-content-section-list--thread">
+function Thread(props: {
+    thread: ApiThread;
+    meta: Accessor<ForumMeta | undefined>;
+}) {
+    const isRead = createMemo(() => {
+        if (!props.meta()?.threads[props.thread.id]) {
+            return false;
+        }
+
+        return props.meta()?.threads[props.thread.id].is_read;
+    });
+    
+    return <a href={`/forums/threads/${props.thread.id}`} class="forum--content-body-content-section-list--thread">
         <div class="forum--content-body-content-section-list--thread-left">
-            <div class="forum--content-body-content-section-list--thread-left-icon">
-                <Fa icon={faCommentAlt} />
+            <div class={`forum--content-body-content-section-list--thread-left-icon ${isRead() ? "forum--content-body-content-section-list--thread-left-icon-read" : ""}`}>
+                <Show when={!isRead()}>
+                    <Fa icon={faCommentAltSolid} />
+                </Show>
+                <Show when={isRead()}>
+                    <Fa icon={faCommentAltRegular}  />
+                </Show>
             </div>
             <div class="forum--content-body-content-section-list--thread-left-info">
-                <span class="forum--content-body-content-section-list--thread-left-info-new" />
+                <Show when={!isRead()}>
+                    <span class="forum--content-body-content-section-list--thread-left-info-new" />
+                </Show>
                 <div class="forum--content-body-content-section-list--thread-left-info-details">
-                    <h1>{props.title}</h1>
-                    <p>by <AccountFlyout account={props.author}>
-                        <a href={`/accounts/${props.author.id}`}>
-                            <Show when={props.author.primary}>
-                                <span style={{ "background-color": props.author.primary!.color }} />
+                    <h1>{props.thread.title}</h1>
+                    <p>by <AccountFlyout account={props.thread.author}>
+                        <a href={`/accounts/${props.thread.author.id}`}>
+                            <Show when={props.thread.author.primary}>
+                                <span style={{ "background-color": props.thread.author.primary!.color }} />
                             </Show>
-                            {props.author.username}
+                            {props.thread.author.username}
                         </a>
                     </AccountFlyout>
                     </p>
@@ -89,15 +123,15 @@ function Thread(props: ApiThread) {
         </div>
         <div class="forum--content-body-content-section-list--thread-right">
             <div class="forum--content-body-content-section-list--thread-right-last_reply">
-                <p>Last reply by <AccountFlyout account={props.last_post?.author}>
-                    <a href={`/accounts/${props.last_post?.author.id}`}>
-                        <Show when={props.last_post?.author.primary}>
-                            <span style={{ "background-color": props.last_post?.author.primary!.color }} />
+                <p>Last reply by <AccountFlyout account={props.thread.last_post?.author}>
+                    <a href={`/accounts/${props.thread.last_post?.author.id}`}>
+                        <Show when={props.thread.last_post?.author.primary}>
+                            <span style={{ "background-color": props.thread.last_post?.author.primary!.color }} />
                         </Show>
-                        {props.last_post?.author.username}
+                        {props.thread.last_post?.author.username}
                     </a>
                 </AccountFlyout></p>
-                <p>{Util.getRelativeTimeString(new Date(props.last_post?.updated_at ?? props.last_post!.created_at!))}</p>
+                <p>{Util.getRelativeTimeString(new Date(props.thread.last_post?.updated_at ?? props.thread.last_post!.created_at!))}</p>
             </div>
             <div class="forum--content-body-content-section-list--thread-right-stats">
                 <p>Replies: 10</p>
